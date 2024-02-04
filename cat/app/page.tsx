c3465602bc654b99375a7705449aad7b44c5fd29
Leonardo Schettini <leonardo.schettini@crayon.com>
2024-02-04 13:41:11 +0100

    refact: sidebar location

--- cat/app/page.tsx
+++ cat/app/page.tsx
@@ -1,55 +1,11 @@
-"use client"
-
-import { Flex } from '@radix-ui/themes';
-import { useState } from 'react';
-import DarkModeSwitch from "./_components/DarkModeSwitch";
-import { ItemInfo, Menu, Sidebar } from "./_components/Sidebar";
+import { Heading } from "@radix-ui/themes";
 
 export default function Home() {
-    // Temporary function to load initial data
-    const loadInitialItems = () => {
-        return [
-            {
-                id: "1", title: "A single page", emoji: "1fae0", subItems: [
-                    { id: "2", title: "A sub page", emoji: undefined },
-                    { id: "3", title: "Another sub page", emoji: undefined }
-                ]
-            },
-            { id: "4", title: "Another single page", emoji: undefined }]
-    }
-    // ------------------------------
-    // Temporary save functionality
-    const savePageItems = (items: any) => {
-        console.log("Saving `Pages` items...")
-        // to string
-        console.log(JSON.stringify(items, null, 2))
-    }
-    // ------------------------------
 
-    const [openPageInfo, setOpenPageInfo] = useState<ItemInfo | undefined>()
-
-    const appPortalId = "app-portal";
-    const menuPropPack = { appPortalId, setOpenPageInfo }
 
     return (
-        <main>
-            <Flex id={appPortalId} style={{ position: "absolute" }} />
-            <Sidebar>
-                <Flex
-                    className="MenuHeader"
-                    style={{ justifyContent: "right", marginBottom: "1em" }}
-                >
-                    <DarkModeSwitch size="1" mode="dark" />
-                </Flex>
-                <Menu
-                    title="Pages"
-                    includeAddButton={true}
-                    menuItems={loadInitialItems()}
-                    saveItems={savePageItems}
-                    {...menuPropPack}
-                />
-                <Menu title="Shared" {...menuPropPack} />
-            </Sidebar>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <Heading className="mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl">Welcome to Cat!</Heading>
         </main>
     );
 }
